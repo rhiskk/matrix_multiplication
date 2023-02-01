c8703c6a7066827bac8a16684ed5729a4ae75dd1
rhiskk <hiski.r@gmail.com>
2023-02-01 09:18:35 +0200

    change matrix calculation order and CDF calculation accuracy to reduce RAM needed

--- run.py
+++ run.py
@@ -12,30 +12,33 @@ def performance_measurement(queue, stop_event):
     memory = []
     cpu = []
     time_stamps = []
-    pid = os.getpid()
     start_time = time.time()
 
     while not stop_event.is_set():
-        memory.append(psutil.Process(pid).memory_info().rss / 1024**2)
+        memory.append(psutil.virtual_memory()[3] / 1024**3)
         cpu.append(psutil.cpu_percent(interval=1))
         time_stamps.append(time.time()-start_time)
         
     queue.put((time_stamps, memory, cpu))
 
-# Generate random matrices and compute D
-def matrix_multiplication():
-    A = np.random.rand(10**4, 10**3)
-    B = np.random.rand(10**3, 10**4)
-    C = np.random.rand(10**4, 1)
-    D = A.dot(B).dot(C)
-    return (A, D)
+# Generate matrices
+def generate_matrices():
+    A = np.random.rand(10**6, 10**3)
+    B = np.random.rand(10**3, 10**6)
+    C = np.random.rand(10**6, 1)
+    return (A, B, C)
+
+# Perform matrix multiplication
+def matrix_multiplication(A, B, C):
+    BC = np.dot(B, C)
+    D = np.dot(A, BC)
+    return D
 
 # Plot the memory and CPU usage over time
 def plot_performance(path, time_stamps, memory, cpu):
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
     ax1.plot(time_stamps, memory,'tab:orange')
-    ax1.set_xlabel('Time (s)')
-    ax1.set_ylabel('Memory Usage (MB)')
+    ax1.set_ylabel('Memory Usage (GB)')
     ax1.set_title('Memory Usage Over Time')
     ax2.plot(time_stamps, cpu, 'tab:green')
     ax2.set_xlabel('Time (s)')
@@ -47,11 +50,13 @@ def plot_performance(path, time_stamps, memory, cpu):
 
 # Plot the CDF of A
 def plot_cdf(path, A):
-    sorted_A = np.sort(A, axis=None)
-    cdf = np.linspace(0, 1, sorted_A.size)
-    plt.plot(sorted_A, cdf, 'tab:blue')
-    plt.xlabel('Value in A')
-    plt.ylabel('CDF')
+    hist, bin_edges = np.histogram(A, bins=1000, density=True)
+    cdf = np.cumsum(hist)
+    y = cdf / cdf[-1]
+    x = bin_edges[1:]
+    plt.plot(x, y, 'tab:blue')
+    plt.xlabel('Value in A less than x')
+    plt.ylabel('Probability')
     plt.title('CDF of A')
     plt.savefig(path + 'cdf.png')
     plt.clf()
@@ -63,7 +68,8 @@ def main():
     process = multiprocessing.Process(target=performance_measurement, args=(queue, stop_event))
     process.start()
 
-    A, D = matrix_multiplication()
+    A, B, C = generate_matrices()
+    D = matrix_multiplication(A, B, C)
 
     # Stop the process
     stop_event.set()
